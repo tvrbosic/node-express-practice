501b12e5be041f6eabd389d407723402413f9532
Tomislav Vrbošić <tomislav.vrbosic@gmail.com>
2024-12-28 18:21:27 +0100

    28_12_2024

--- middlewares/errorHandling.js
+++ middlewares/errorHandling.js
@@ -1,10 +1,49 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  return new AppError(`Invalid ${err.path}: ${err.value}.`, 400);
+};
+
+const sendErrorInDevelopment = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorInProduction = (err, res) => {
+  // Operational, expected error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // Programming or other unexpected error: do not send error details to client
+    console.error('ERROR: ', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Internal server error occured.',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorInDevelopment(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
+
+    sendErrorInProduction(error, res);
+  }
+
   next();
 };
